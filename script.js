1d2a267dd1c9b0636ccf4338ea43c5a4fce02677
Stephen <sagelvl40@gmail.com>
2024-07-08 10:28:38 +0100

    keep it simple stupid

---
+++ script.js
@@ -0,0 +1,23 @@
+function setBgBlue() {
+  document.body.style.backgroundColor = "blue";
+}
+
+function setBgPurple() {
+  document.body.style.backgroundColor = "purple";
+}
+
+function setBgGreen() {
+  document.body.style.backgroundColor = "green";
+}
+
+function setBgYellow() {
+  document.body.style.backgroundColor = "yellow";
+}
+
+function setBgRed() {
+  document.body.style.backgroundColor = "red";
+}
+
+function defaultBgColor() {
+  document.body.style.backgroundColor = "white";
+}
